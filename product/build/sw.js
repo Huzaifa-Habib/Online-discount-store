8be6e634e743a1925d097a1b05a30926c944ea95
cocobutte <cocobutter128@gmail.com>
2023-03-07 19:47:48 +0500

    update

--- product/build/sw.js
+++ product/build/sw.js
@@ -1,12 +1,12 @@
-// This is the "Offline copy of pages" service worker
+// // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
+// const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
 
